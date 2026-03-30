cd0747677c99c09360c6873faabd0a8760080140
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-03-30 14:01:43 +0300

    setup jupyter notebook and models module

---
+++ models.py
@@ -0,0 +1,4 @@
+class LogisticRegression:
+    # the general formula is $y=frac{1}{1+e^{-x}}$
+    # train the model to determine the position of the halfway proability in a way that minimizes the classification error
+    pass
